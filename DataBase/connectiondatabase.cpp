e72d08250db4eef40cf237afef8b72bb38c06a5b
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2017-03-16 14:03:46 +0100

    update from windows

--- DataBase/connectiondatabase.cpp
+++ DataBase/connectiondatabase.cpp
@@ -7,8 +7,11 @@ connectionDataBase::connectionDataBase()
     // Connection à la base de donnée de connection
     m_db = QSqlDatabase::addDatabase("QSQLITE");
 
+#ifdef Q_OS_WIN
+    m_db.setDatabaseName("F:/Programmes/Programme_Alisson/ConnectionDataBase/connectionDB.db");
+#else
     m_db.setDatabaseName("/Users/Guillaume/softOsteo/ConnectionDataBase/connectionDB.db");
-
+#endif
 
     // vérification que la connection à pue être établie
     checkConnectionToDatabase();
